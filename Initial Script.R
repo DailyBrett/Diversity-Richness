b4fe3b076ea199270848b7d42f37bac5300c00ce
DailyBrett <bnpeterson@uchicago.edu>
2024-01-10 22:33:41 -0600

    Resubmission to fix an error

--- Initial Script.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse) #Loading first package
-library(papaja) #Loading second package
-library(psych) #Loading third package
-city_numbers <- c(1, 2, 3) 
-Texas_cities <- c("Dallas", "Houston", "Austin")
-city_number_frame <- data_frame(City= Texas_cities, Order=city_numbers)
-
