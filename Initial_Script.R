8eb187f4a7f8771ed1d21b36e99bf967077f9bfd
DailyBrett <bnpeterson@uchicago.edu>
2024-01-10 22:29:32 -0600

    I wrote a script and made an introductory R notebook.

---
+++ Initial_Script.R
@@ -0,0 +1,7 @@
+library(tidyverse) #Loading first package
+library(papaja) #Loading second package
+library(psych) #Loading third package
+city_numbers <- c(1, 2, 3) 
+Texas_cities <- c("Dallas", "Houston", "Austin")
+city_number_frame <- tibble(City= Texas_cities, Order=city_numbers) #Received an error using dataframe so change to tibble
+
