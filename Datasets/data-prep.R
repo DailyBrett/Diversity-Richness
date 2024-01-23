8d12ce2c86acbdf66cdaf5802ae1399da6a7b11f
DailyBrett <bnpeterson@uchicago.edu>
2024-01-22 19:56:46 -0600

    Made a mock table to provide a tmeplate of the kind of infor I will need, made a data-prep script with steps for formatting my data @nrdowling @ysh627

---
+++ Datasets/data-prep.R
@@ -0,0 +1,20 @@
+# -----------------------------------------------------
+# Data Prep: Plan
+# -----------------------------------------------------
+
+# 1) I need to download a dataset to for US zip codes as city was determine to be too large of a measure
+# 2) I need to format this zip code data in a similar fashion to the city day
+# In order to achieve this aim, I will change headings, remove empty columns, and move the diversity data to the front spot
+# 3) I will need to write a function that that determines a diversity score based on the racial demographics in each zip code
+# This function will determine the probability of pulling members of two different races from a zip code. I currently do not know how to write such a function. 
+# 4) I will then create a new column to hold all of these different diversity scores.
+# 5) I will then need to download data from the psychological richness study, this data will the scores of a group of participants to different well-being measures, as well as their demographic data such as zip code.
+# 6) I will then merge these two data sets, such that the columns are added to include the corresponding demographic information for each person's zip scores
+# F0r merging the data, I am not quite sure yet whether I should first make blank spaces in one data set to copy into from the other, or instead fully merge the two data sets into one
+
+# -----------------------------------------------------
+# Additional Questions
+# -----------------------------------------------------
+
+#  I am not sure if I need to change the first column to make it more fully tidy-formatted. The first column is not repeating so may need to re-arrange that. May need to shift to pit longer in this case
+# May need to do some additional data cleaning like simplying measurements for zip code income, and maybe removing totals and just retaining percentage data
