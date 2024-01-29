82c74e102d1b0fcce2cc6f221c8646284d628ced
DailyBrett <bnpeterson@uchicago.edu>
2024-01-29 16:11:34 -0600

    Assignment 10: Wrote instructions for future plots and created chunks and smaples of them within Exploring R @nrdowling @ysh627

--- Datasets/data-prep.R
+++ Datasets/data-prep.R
@@ -13,8 +13,20 @@
 # F0r merging the data, I am not quite sure yet whether I should first make blank spaces in one data set to copy into from the other, or instead fully merge the two data sets into one
 
 # -----------------------------------------------------
+# Plots Prep
+# -----------------------------------------------------
+# 1) I want to make a scatter plot showing the comparison of the zip-code diversity (the x axis) with the individual psych richness score (the y axis).
+# In this chart, I would like clear labels, of the two variables. As well as visible  trend line demonstrating the relationship between the two. (such as that demonstrated in the mock up table)
+# 2) I also want a line plot similarly demonstrating the relationship between the zip-code diversity (the x axis) with the individual psych richness score (the y axis).
+# 3) I will make a histogram grouping the participants into 10 bins based on their diversity scores, with y axis being psych richness cores
+# 4) Last I will make several triangle shped graphics demonstrating the potential mediators (such as income-inequality, population density, or access to cultural experiences) to demonstrated direct versus indirect affects
+# To do so I would need to run several regressions models including mediators.
+
+# -----------------------------------------------------
 # Additional Questions
 # -----------------------------------------------------
 
 #  I am not sure if I need to change the first column to make it more fully tidy-formatted. The first column is not repeating so may need to re-arrange that. May need to shift to pit longer in this case
 # May need to do some additional data cleaning like simplying measurements for zip code income, and maybe removing totals and just retaining percentage data
+
+
